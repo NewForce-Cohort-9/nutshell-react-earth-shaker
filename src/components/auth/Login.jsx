b4ea4a3abda4e4869d967a98b913361585d1ba8a
Meowph <bpardue33@gmail.com>
2024-06-05 09:18:48 -0400

    added files to folders

---
+++ src/components/auth/Login.jsx
@@ -0,0 +1,66 @@
+import /*React,*/ { useState } from "react"
+import { Link } from "react-router-dom"
+import { useNavigate } from "react-router-dom"
+import "./Login.css"
+import { getUserByEmail } from "../../services/userService"
+
+export const Login = () => {
+  const [email, set] = useState("barry@gmail.com")
+  const navigate = useNavigate()
+
+  const handleLogin = (e) => {
+    e.preventDefault()
+
+    //Get user by email from database, then store user in local storage
+    getUserByEmail(email).then((foundUsers) => {
+      if (foundUsers.length === 1) {
+        const user = foundUsers[0]
+        localStorage.setItem(
+          "nutshell_user",
+          JSON.stringify({
+            id: user.id,
+            /*isStaff: user.isStaff,*/
+          })
+        )
+        //then it will navigate to home
+        navigate("/")
+      } else {
+        window.alert("Invalid login")
+      }
+    })
+  }
+
+  return (
+    <main className="container-login">
+      <section>
+        <form className="form-login" onSubmit={handleLogin}>
+          <h1>Welcome to Nutshell!</h1>
+          <h2>Please sign in</h2>
+          <fieldset>
+            <div className="form-group">
+              <input
+                type="email"
+                value={email}
+                onChange={(evt) => set(evt.target.value)}
+                className="form-control"
+                placeholder="Email address"
+                required
+                autoFocus
+              />
+            </div>
+          </fieldset>
+          <fieldset>
+            <div className="form-group">
+              <button className="login-btn btn-info" type="submit">
+                Sign in
+              </button>
+            </div>
+          </fieldset>
+        </form>
+      </section>
+      <section>
+        <Link to="/register">Not a member yet?</Link>
+      </section>
+    </main>
+  )
+}
